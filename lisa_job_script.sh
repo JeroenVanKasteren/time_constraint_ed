2d75bdb1c0ec7ee38eb27e5b0aed5ea3aec2f4a3
Jeroen <jeroenvkasteren@gmail.com>
2023-04-25 09:47:52 +0200

    minor job script modifications

--- lisa_job_script.sh
+++ lisa_job_script.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #Set job requirements & Experiment with increasing cpus-per-task
 #SBATCH --job-name "time_constraint_ED"
-#SBATCH --partition short
+#SBATCH --partition shared
 #SBATCH --ntasks 1
-#SBATCH --cpus-per-task 1
+#SBATCH --cpus-per-task 8
 #time >1 minute
-#SBATCH --time 01:01:00
-#SBATCH --array 1-3
+#SBATCH --time 20:01:00
+#SBATCH --array 1-100
 #SBATCH --output Results/prints_%A_%a.out
 #SBATCH --error Results/prints_%A_%a.err
 
-python Train.py --id $SLURM_JOBID --index $SLURM_ARRAY_TASK_ID --J 2 --gamma 30 --policy False --time "01:00:30"
+python Train.py --id $SLURM_JOBID --index $SLURM_ARRAY_TASK_ID --J 2 --gamma 30 --policy False --time "19:30:00"
