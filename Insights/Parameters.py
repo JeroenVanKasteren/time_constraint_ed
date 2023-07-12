0cdfc320f783746daaa53be53d6b261843ff720a
Jeroen <jeroenvkasteren@gmail.com>
2023-07-12 15:48:06 +0200

    paramgrid to instance file and result file.

--- Insights/Parameters.py
+++ Insights/Parameters.py
@@ -1,5 +1,7 @@
 """
 Creation of file with instances of the Time ConstraintEDs problem.
+ID of instance is the ID of the instance file combined with the index of the
+instance in the file.
 
 @author: Jeroen van Kasteren (jeroen.van.kasteren@vu.nl)
 Created on 31-5-2023.
@@ -26,26 +28,14 @@ MU_1_GRID = [1/3]
 MU_2_GRID = np.array([1, 1.5, 2])*MU_1_GRID
 LOAD_GRID = [0.5, 0.6, 0.7, 0.8]  # 0.9?
 LOAD_IMB = [1/3, 1, 3]
-
-instances_path = 'Insights/instances.csv'
+# TODO how to reach other folder with path!?
+instances_path = 'results/instances_01.csv'
 instance_columns = ['J', 'S', 'D', 'size', 'size_i',
-                    'gamma', 'eps', 't', 'c', 'r',
-                    'lambda', 'mu', 'load',
-                    'attempts_VI', 'attempts_OSPI',
-                    'vi_g', 'vi_time', 'vi_iter',
-                    'ospi_g', 'ospi_time', 'ospi_iter',
-                    'rel_gap', 'abs_gap']
+                    'gamma', 'e', 't', 'c', 'r',
+                    'lambda', 'mu', 'load', 'target_prob',
+                    'vi_attempts', 'vi_time', 'vi_iter', 'vi_solved',
+                    'ospi_attempts', 'ospi_time', 'ospi_iter', 'ospi_solved']
 
-results_path = 'Results/instances.csv'
-results_columns = ['instance',
-                   'job_id_vi', 'array_id_vi', 'solve_date_vi',
-                   'job_id_ospi', 'array_id_ospi', 'solve_date_ospi',
-                   'J', 'S', 'D', 'size', 'size_i',
-                   'gamma', 'eps', 't', 'c', 'r',
-                   'lambda', 'mu', 'load', 'target_prob',
-                   'vi_g', 'vi_time', 'vi_iter',
-                   'ospi_g', 'ospi_time', 'ospi_iter',
-                   'rel_gap', 'abs_gap']
 MAX_TARGET_PROB = 0.9
 
 param_grid = {'S': S_GRID,
@@ -89,16 +79,23 @@ print(grid[grid['target_prob'] > MAX_TARGET_PROB])
 grid = grid[grid['target_prob'] > MAX_TARGET_PROB]
 
 # Derive solved from value for g.
-grid['attempts_VI'] = 0
-grid['attempts_OSPI'] = 0
-grid['vi_g'] = np.nan
+grid['vi_attempts'] = 0
 grid['vi_time'] = np.nan
 grid['vi_iter'] = np.nan
-grid['ospi_g'] = np.nan
+grid['vi_solved'] = False
+grid['ospi_attempts'] = 0
 grid['ospi_time'] = np.nan
 grid['ospi_iter'] = np.nan
+grid['ospi_solved'] = False
 
 grid = grid[instance_columns]
+
+# TODO how to reach other folder with path!?
+os.listdir()
+if os.path.isfile(instances_path):
+    print('file already exists, name: ', instances_path)
+else:
+    grid.to_csv(instances_path)
 
 # If results file already exists
 #
@@ -113,39 +110,35 @@ grid = grid[instance_columns]
 #     reader = csv.reader(csv_file)
 #     mydict = dict(reader)
 
-
-
-MAX_TARGET_PROB = 0.9
-
-def load_args(raw_args=None):
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--J', default=2)  # User input
-    parser.add_argument('--gamma', default=10)  # User input
-    args = parser.parse_args(raw_args)
-    args.id = int(args.id)
-    args.index = int(args.index)
-    args.J = int(args.J)
-    args.gamma = float(args.gamma)
-    args.policy = args.policy == 'True'
-    return args
-
-def main(raw_args=None):
-    Path(FILEPATH).touch()
-    parameters = pd.read_csv(FILEPATH, names=COLUMNS)
-    if os.stat(FILEPATH).st_size == 0:
-        with open(FILEPATH, 'r') as f:
-
-    args = load_args(raw_args)
-    # ---- Problem ---- #
-    seed = args.id * args.index
-    f_name = 'Results/' + str(args.id) + '_' + str(args.index) + 'Py.txt'
-    rho = 1
-    smu = 0
-    while smu * (1 - rho) < -np.log(MAX_TARGET_PROB):
-        env = Env(J=args.J, gamma=args.gamma, P=1e3, e=5e-4, seed=seed,
-                  max_time=args.time, convergence_check=20, print_modulo=100,
-                  b_out_f=args.b_out_f, out_f=f_name)
-        smu = env.S * sum(env.lab) / sum(env.lab / env.mu)
-        rho = env.load
-        seed += 1
-    pi_learner = PolicyIteration()
+# def load_args(raw_args=None):
+#     parser = argparse.ArgumentParser()
+#     parser.add_argument('--J', default=2)  # User input
+#     parser.add_argument('--gamma', default=10)  # User input
+#     args = parser.parse_args(raw_args)
+#     args.id = int(args.id)
+#     args.index = int(args.index)
+#     args.J = int(args.J)
+#     args.gamma = float(args.gamma)
+#     args.policy = args.policy == 'True'
+#     return args
+#
+# def main(raw_args=None):
+#     Path(FILEPATH).touch()
+#     parameters = pd.read_csv(FILEPATH, names=COLUMNS)
+#     if os.stat(FILEPATH).st_size == 0:
+#         with open(FILEPATH, 'r') as f:
+#
+#     args = load_args(raw_args)
+#     # ---- Problem ---- #
+#     seed = args.id * args.index
+#     f_name = 'Results/' + str(args.id) + '_' + str(args.index) + 'Py.txt'
+#     rho = 1
+#     smu = 0
+#     while smu * (1 - rho) < -np.log(MAX_TARGET_PROB):
+#         env = Env(J=args.J, gamma=args.gamma, P=1e3, e=5e-4, seed=seed,
+#                   max_time=args.time, convergence_check=20, print_modulo=100,
+#                   b_out_f=args.b_out_f, out_f=f_name)
+#         smu = env.S * sum(env.lab) / sum(env.lab / env.mu)
+#         rho = env.load
+#         seed += 1
+#     pi_learner = PolicyIteration()
