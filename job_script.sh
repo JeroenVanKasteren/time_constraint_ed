9b97bad2bd9c3540ee6ae014e0e63feed416675b
JeroenVanKasteren <jeroenvkasteren@gmail.com>
2023-08-15 23:03:57 +0200

    Generating parameters to instances and loading in results.

--- job_script.sh
+++ job_script.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 #SBATCH --job-name "time_constraint_ED"
-#SBATCH --ntasks 1
-#SBATCH --cpus-per-task 16
-#SBATCH --time 0-00:01:00
-#SBATCH --array 1-1
+#SBATCH --nodes 1
+#SBATCH --time 0-03:00:00
+#SBATCH --array 1-10
 #SBATCH --output results/read/prints_%A_%a.out
 #SBATCH --error results/read/prints_%A_%a.err
 
 # time is rounded to minutes
 TIMELIMIT=`squeue -j $SLURM_JOB_ID -o "%l" | tail -1`
 
-python train.py --job_id $SLURM_JOBID --array_id $SLURM_ARRAY_TASK_ID --time $TIMELIMIT --method ospi
+python train.py --job_id $SLURM_JOBID --array_id $SLURM_ARRAY_TASK_ID --time $TIMELIMIT --instance 01 --method vi
