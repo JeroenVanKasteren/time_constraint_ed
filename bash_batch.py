65d4c06111cfbabf54fb181412843cbe531bfbda
laptop <jeroen.van.kasteren@vu.nl>
2024-07-16 12:57:59 +0200

    bash batch files

---
+++ bash_batch.py
@@ -0,0 +1,33 @@
+from subprocess import run
+
+JOBSCRIPT = """#!/bin/bash
+#SBATCH --job-name {name}
+#SBATCH --cpus-per-task 16
+#SBATCH --time {time}
+#SBATCH --array 1-{n_array}
+#SBATCH --output results/read/prints_%A_%a.out
+#SBATCH --error results/read/prints_%A_%a.err
+
+# time is rounded to minutes
+TIMELIMIT=`squeue -j $SLURM_JOB_ID -o "%l" | tail -1`
+
+python train.py --job_id $SLURM_JOBID --array_id $SLURM_ARRAY_TASK_ID --time $TIMELIMIT --instance {instance} --method {method}
+# python read_results_sim.py
+"""
+
+instances = [['J1', '0-00:20:00', '108'],
+             ['J2', '0-03:00:00', '108'],
+             ['J2_D_gam', '0-03:00:00', '216']]
+methods = ['vi', 'ospi', 'sdf', 'fcfs', 'pi', 'cmu_t_min']
+
+for instance, time, n_array in instances:
+    for method in methods:
+        script = JOBSCRIPT.format(
+            name=instance + '_solve_ED',
+            time=time,
+            n_array=n_array,
+            instance=instance,
+            method=method)
+
+        run(["sbatch"], input=script.encode())
+        # print(script.encode())
---
+++ bash_batch_sim.py
@@ -0,0 +1,31 @@
+from subprocess import run
+
+JOBSCRIPT = """#!/bin/bash
+#SBATCH --job-name {name}
+#SBATCH --cpus-per-task 16
+#SBATCH --time {time}
+#SBATCH --array 1-{n_array}
+#SBATCH --output results/read/prints_%A_%a.out
+#SBATCH --error results/read/prints_%A_%a.err
+
+# time is rounded to minutes
+TIMELIMIT=`squeue -j $SLURM_JOB_ID -o "%l" | tail -1`
+
+python run_simulations.py --job_id $SLURM_JOBID --array_id $SLURM_ARRAY_TASK_ID --time $TIMELIMIT --instance {instance} --max_iter 1e6 --continue_run True
+# python read_results_sim.py
+"""
+
+instances = [['J1', '0-00:20:00', '108'],
+             ['J2', '0-00:20:00', '108'],
+             ['J2_D_gam', '0-00:20:00', '216'],
+             ['sim', '0-00:20:00', '10']]
+
+for instance, time, n_array in instances:
+    script = JOBSCRIPT.format(
+        name=instance + '_sim_ED',
+        time=time,
+        n_array=n_array,
+        instance=instance)
+
+    run(["sbatch"], input=script.encode())
+    # print(script.encode())
